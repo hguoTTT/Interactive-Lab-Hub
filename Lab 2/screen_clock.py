e81e1e87bfad97320f0a6125167bf1200907ba2f
Hanchen Guo <50841018+hguoTTT@users.noreply.github.com>
2021-09-23 10:04:00 -0400

    Update screen_clock.py
    test background 2

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -72,7 +72,7 @@ buttonB.switch_to_input()
 
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 255))
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 255, 0))
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
     str = strftime("%m/%d/%Y %H:%M:%S")

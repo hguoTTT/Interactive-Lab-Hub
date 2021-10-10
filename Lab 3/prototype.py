ec8fa42fcd62952c5559ffaff0526bed3b2b34f3
Hanchen Guo <50841018+hguoTTT@users.noreply.github.com>
2021-10-09 21:31:53 -0400

    Update prototype.py
    update tts

--- Lab 3/prototype.py
+++ Lab 3/prototype.py
@@ -2,12 +2,15 @@ from gtts import gTTS
 import os
   
 correctText = 'Congratulations, you are correct.'
+incorrectText = 'Try again.'
   
 language = 'en'
-myobj = gTTS(text=correctText, lang=language, slow=False)
-  
+correct = gTTS(text=correctText, lang=language, slow=False)
+inccorect = gTTS(text=incorrectText, lang=language, slow=False) 
+
 # Saving the converted audio in a mp3 file named
-myobj.save("correct.mp3")
+correct.save("correct.mp3")
+incorrect.save("incorrect.mp3")
   
 # Playing the converted file
 os.system("mpg321 correct.mp3")

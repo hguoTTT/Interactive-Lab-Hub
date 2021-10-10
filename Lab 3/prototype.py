1125f86698c3ef60934b07cf6bd649d418c928b1
Hanchen Guo <50841018+hguoTTT@users.noreply.github.com>
2021-10-09 21:23:44 -0400

    Update prototype.py
    add in tts

--- Lab 3/prototype.py
+++ Lab 3/prototype.py
@@ -1,1 +1,13 @@
-
+from gtts import gTTS
+import os
+  
+correctText = 'Congratulations, you are correct.'
+  
+language = 'en'
+myobj = gTTS(text=correctText, lang=language, slow=False)
+  
+# Saving the converted audio in a mp3 file named
+myobj.save("correct.mp3")
+  
+# Playing the converted file
+os.system("mpg321 correct.mp3")

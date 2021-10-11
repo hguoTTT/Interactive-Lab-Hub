8e6d09cef83df1fc35bdcb9f3289b250ede1b44e
Hanchen Guo <50841018+hguoTTT@users.noreply.github.com>
2021-10-11 13:19:38 -0400

    Update prototype.py
    add main

--- Lab 3/prototype.py
+++ Lab 3/prototype.py
@@ -13,4 +13,21 @@ correct.save("correct.mp3")
 incorrect.save("incorrect.mp3")
   
 # Playing the converted file
-os.system("mplayer correct.mp3")
+#os.system("mplayer correct.mp3")
+
+def main()
+  a = random.randint(5,9)
+  b = random.randint(0,5)
+  c = random.randint(0,1)
+  d = 0
+  if c == 0:
+    questionText = "What is " + str(a) + " minus " + str(b)
+    answer = a-b
+  else:
+    questionText = "What is " + str(a) + " plus " + str(b)
+    answer = a+b
+  question = gTTS(text=questionText, lang=language, slow=False)
+  qeustion.save("question.mp3")
+  os.system("mplayer question.mp3")
+
+main()

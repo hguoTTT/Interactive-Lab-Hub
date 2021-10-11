ab1a64117133812d58fcafff2050bfe0fb91388c
Hanchen Guo <50841018+hguoTTT@users.noreply.github.com>
2021-10-11 14:29:12 -0400

    Update prototype.py
    update speech2text

--- Lab 3/prototype.py
+++ Lab 3/prototype.py
@@ -1,4 +1,6 @@
 from gtts import gTTS
+from word2number import w2n
+import speech_recognition as sr
 import os
 import random
   
@@ -16,6 +18,9 @@ incorrect.save("incorrect.mp3")
 # Playing the converted file
 #os.system("mplayer correct.mp3")
 
+#Set up speech recognition
+r = sr.Recognizer()
+
 def main():
   a = random.randint(5,9)
   b = random.randint(0,5)
@@ -27,8 +32,27 @@ def main():
   else:
     questionText = "What is " + str(a) + " plus " + str(b)
     answer = a+b
-  question = gTTS(text=questionText, lang=language, slow=False)
-  question.save("question.mp3")
-  os.system("mplayer question.mp3")
+  while true:
+    question = gTTS(text=questionText, lang=language, slow=False)
+    question.save("question.mp3")
+    os.system("mplayer question.mp3")
+    with sr.Microphone() as source:
+      audio = r.listen(source)
+    response = r.recognize_sphinx(audio)
+    responseInt = w2n.word_to_num(response)
+    if reponseInt == answer:
+      os.system("mplayer correct.mp3")
+      a = random.randint(5,9)
+      b = random.randint(0,5)
+      c = random.randint(0,1)
+      d = 0
+      if c == 0:
+        questionText = "What is " + str(a) + " minus " + str(b)
+        answer = a-b
+      else:
+        questionText = "What is " + str(a) + " plus " + str(b)
+        answer = a+b
+    else:
+      os.system("mplayer incorrect.mp3")
 
 main()

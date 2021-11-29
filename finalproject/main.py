1226ad73265f15652f139fb9711dfdacf2f8ab96
Hanchen Guo <50841018+hguoTTT@users.noreply.github.com>
2021-11-29 14:12:43 -0500

    Update main.py
    update barebones code

--- finalproject/main.py
+++ finalproject/main.py
@@ -1,1 +1,20 @@
+import pygame
 
+pygame.init()
+pygame.mixer.init()
+
+def play(song):
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play()
+def stop():
+    pygame.mixer.music.stop()
+def pause():
+    pygame.mixer.music.pause()
+def unpause():
+    pygame.mixer.music.unpause()
+    
+def main():
+    song = "test.mp3"
+    while True:
+       
+main()

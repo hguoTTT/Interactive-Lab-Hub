b56201eec99a98cd067c60f473d91b60d4cd5406
Hanchen Guo <50841018+hguoTTT@users.noreply.github.com>
2021-11-29 20:13:29 -0500

    Update main.py
    added new library and pi code

--- finalproject/main.py
+++ finalproject/main.py
@@ -1,9 +1,74 @@
 import pygame
+import board
+import busio
+import adafruit_apds9960.apds9960
+import time
+import paho.mqtt.client as mqtt
+import uuid
+import signal
+
+from gtts import gTTS
+from word2number import w2n
+import speech_recognition as sr
+import os
+import random
+
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+# ======================= Controls ==========================================
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+
+image = Image.new("RGB", (width, height))
+draw = ImageDraw.Draw(image)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
+
+sensor.enable_color = True
+r, g, b, a = sensor.color_data
+
+#================================ music player ===============================================
 
 pygame.init()
 pygame.mixer.init()
 
-def play(song):
+def play(x):
     pygame.mixer.music.load(song)
     pygame.mixer.music.play()
 def stop():
@@ -13,8 +78,30 @@ def pause():
 def unpause():
     pygame.mixer.music.unpause()
     
-def main():
-    song = "test.mp3"
-    while True:
-       
-main()
+song = "test.mp3"
+play(song)
+
+#Set up speech recognition
+r = sr.Recognizer()
+    
+while True:
+    string = song
+    
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+    
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    
+    # there's a few things going on here 
+    # colors are reported at 16bits (thats 65536 levels per color).
+    # we need to convert that to 0-255. thats what the 255*(x/65536) is doing
+    # color are also reported with an alpha (opacity, or in our case a proxy for ambient brightness)
+    # 255*(1-(a/65536)) acts as scaling factor for brightness, it worked well enough in the lab but 
+    # your success may vary depenging on how much ambient light there is, you can mess with these constants
+
+    # if we press the button, send msg to cahnge everyones color
+    if not buttonA.value:
+        pause()
+    draw.text((0, 0), string, font=font, fill=(240,255,255))
+    
+    disp.image(image, 90)
+    time.sleep(.01)
